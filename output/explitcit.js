126ce2a6798ba1ad5bd2f543f1290a16737a9483
AsadulIslamRipon <mdasadulripon@gmail.com>
2023-08-24 01:50:01 +0600

    explicit implicit union

---
+++ output/explitcit.js
@@ -0,0 +1,18 @@
+"use strict";
+let a;
+let b;
+a = "Ripon";
+b = 50;
+let c;
+c = "Ripon";
+let d = [];
+d.push("Ripon", 50);
+//Object
+let Name;
+Name = {
+    name: "Ripon",
+    age: 50,
+    adult: true
+};
+let o;
+o = ["JK", 47];
---
+++ src/explitcit.ts
@@ -0,0 +1,29 @@
+let a:string;
+let b:number;
+
+a="Ripon";
+b=50;
+
+let c:string | number;
+
+
+c ="Ripon"
+
+let d:(string | number)[] = [];
+d.push("Ripon", 50)
+
+//Object
+let Name : {
+    name: string,
+    age: number;
+    adult: boolean;
+}
+
+Name = {
+    name:"Ripon",
+    age: 50,
+    adult: true
+}
+
+let o: object;
+o=["JK", 47]
