fad9ea9baafee60cc4ca8820e2d8b8bf7aa5c7af
AsadulIslamRipon <mdasadulripon@gmail.com>
2023-08-24 01:57:40 +0600

    dynamic or any type

---
+++ output/Dynamic Any.js
@@ -0,0 +1,13 @@
+"use strict";
+let a;
+a = 5;
+a = "Ripon";
+let dynamic = [];
+dynamic.push("Ripon");
+dynamic.push(50);
+///=Object
+let d;
+d = {
+    name: "Ripon",
+    age: 50
+};
---
+++ src/Dynamic Any.ts
@@ -0,0 +1,18 @@
+let a:any;
+
+a = 5;
+a = "Ripon"
+
+let dynamic:any[] = [];
+dynamic.push("Ripon");
+dynamic.push(50);
+
+///=Object
+let d:{
+    name: any;
+    age: any;
+}
+d={
+    name: "Bangladesh",
+    age: 50
+}
